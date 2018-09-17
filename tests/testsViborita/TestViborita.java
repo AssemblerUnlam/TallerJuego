37ab7458fc0154c43b6ab0bb8b25378d6a29baa2
fns92 <sanchez36822171@gmail.com>
2018-09-17 16:36:15 -0300

    fns92: añadiendo primeros ajustes

---
+++ tests/testsViborita/TestViborita.java
@@ -0,0 +1,14 @@
+package testsViborita;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestViborita {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
