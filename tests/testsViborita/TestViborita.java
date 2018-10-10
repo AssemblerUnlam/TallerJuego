fa7f7518b5062762cdd037c3372bbdcef73a1db9
Leonel San Martin <31077603+leo9210@users.noreply.github.com>
2018-10-10 15:47:43 -0300

    Test : se mueve a derecha y hacia abajo

--- tests/testsViborita/TestViborita.java
+++ tests/testsViborita/TestViborita.java
@@ -1,14 +1,28 @@
-package testsViborita;
+import java.awt.Point;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
+import Snake.Snake;
+
 public class TestViborita {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void seMueveDerecha() {
+		Snake snake = new Snake();
+		snake.InitializeSnake(new Point(0,0));
+		snake.moveToRight();
+		snake.moveToMyOwnDirection();
+		Assert.assertEquals(new Point(1,0), snake.getPosition());
 	}
-
+	
+	@Test
+	public void seMueveHaciaAbajo() {
+		Snake snake = new Snake();
+		snake.InitializeSnake(new Point(0,0));
+		snake.moveToDown();
+		snake.moveToMyOwnDirection();
+		Assert.assertEquals(new Point(0,1), snake.getPosition());		
+	}
+	
 }
