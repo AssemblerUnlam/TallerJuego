518510f7ee9811698bd5deb5fa2781745a844d77
fns92 <sanchez36822171@gmail.com>
2018-10-11 16:11:23 -0300

    fns92: Refactorizando los test con before

--- tests/testsViborita/TestViborita.java
+++ tests/testsViborita/TestViborita.java
@@ -3,6 +3,7 @@ package testsViborita;
 import java.awt.Point;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import Fruit.Fruit;
@@ -11,10 +12,26 @@ import Map.Map;
 import Snake.Snake;
 
 public class TestViborita {
+
+	
+	Snake snake, snake2;
+	GameManager game;
+	Map map, map2;
+	Fruit fruit;
+	
+	@Before
+	public void setUp(){
+		
+		snake = new Snake();
+		snake2 = new Snake();
+		game = new GameManager();
+		map = new Map(1000, 1000);
+		map2 = new Map(800, 600);
+		fruit = new Fruit(new Point(1,0));
+	}
 	
 	@Test
-	public void moveToRight() {
-		Snake snake = new Snake();
+	public void moveToRight(){
 		snake.InitializeSnake(new Point(0,0));
 		snake.moveToRight();
 		snake.moveToMyOwnDirection();
@@ -23,7 +40,6 @@ public class TestViborita {
 	
 	@Test
 	public void moveToDown() {
-		Snake snake = new Snake();
 		snake.InitializeSnake(new Point(0,0));
 		snake.moveToDown();
 		snake.moveToMyOwnDirection();
@@ -32,7 +48,6 @@ public class TestViborita {
 	
 	@Test
 	public void moveToUp() {
-		Snake snake = new Snake();
 		snake.InitializeSnake(new Point(10,10));
 		snake.moveToUp();
 		snake.moveToMyOwnDirection();
@@ -41,7 +56,6 @@ public class TestViborita {
 	
 	@Test
 	public void moveToLeft() {
-		Snake snake = new Snake();
 		snake.InitializeSnake(new Point(10,10));
 		snake.moveToLeft();
 		snake.moveToMyOwnDirection();
@@ -50,66 +64,58 @@ public class TestViborita {
 	
 	@Test
 	public void noColisionaConsigoMisma() {
-		Snake player = new Snake();
-		player.InitializeSnake(new Point(50, 50));
-		player.moveToRight();
-		player.moveToMyOwnDirection();
-		player.moveToDown();
-		player.moveToMyOwnDirection();
-		player.moveToLeft();
-		player.moveToMyOwnDirection();
-		player.moveToUp();
-		player.moveToMyOwnDirection();
-		Assert.assertEquals(new Point(50, 50), player.getPosition());
-		Assert.assertEquals(false, player.isDead());
+		snake.InitializeSnake(new Point(50, 50));
+		snake.moveToRight();
+		snake.moveToMyOwnDirection();
+		snake.moveToDown();
+		snake.moveToMyOwnDirection();
+		snake.moveToLeft();
+		snake.moveToMyOwnDirection();
+		snake.moveToUp();
+		snake.moveToMyOwnDirection();
+		Assert.assertEquals(new Point(50, 50), snake.getPosition());
+		Assert.assertEquals(false, snake.isDead());
 	}
 	
 	@Test
 	public void intentaIrHaciaAtras() {
-		Snake player = new Snake();
-		player.InitializeSnake(new Point(10, 10));
-		player.moveToUp();
-		player.moveToMyOwnDirection();
-		Assert.assertEquals(new Point(10, 11), player.getPosition());
+		snake.InitializeSnake(new Point(10, 10));
+		snake.moveToUp();
+		snake.moveToMyOwnDirection();
+		Assert.assertEquals(new Point(10, 11), snake.getPosition());
 	}
 	
-	@Test
-	public void muereAlSalirDelMapa() {
-		GameManager game = new GameManager();
-		Map stage = new Map(1000, 1000);
-		Snake player = new Snake();
-		stage.addSnake();
-		player.InitializeSnake(new Point(0 , 1000));
-		player.moveToMyOwnDirection();
-		game.CheckCollision(player, 1);
-		Assert.assertEquals(true, player.isDead());
-	}
-	
-	@Test
-	public void incrementaSuTamañoAlComerUnaFruta() {
-		GameManager game = new GameManager();
-		Snake player = new Snake();
-		Fruit fruit = new Fruit(new Point(0, 1 ));
-		player.InitializeSnake(new Point(0, 0));
-		player.moveToMyOwnDirection();
-		game.CheckCollision(player, 1);
-		Assert.assertEquals(4, player.getTailLenght());
-	}
+//	@Test
+//	public void muereAlSalirDelMapa() {
+//		stage.addSnake();
+//		player.InitializeSnake(new Point(0 , 1000));
+//		player.moveToMyOwnDirection();
+//		game.CheckCollision(player, 1);
+//		Assert.assertEquals(true, player.isDead());
+//	}
+//	
+//	@Test
+//	public void incrementaSuTamañoAlComerUnaFruta() {
+//
+//		Snake player = new Snake();
+//		Fruit fruit = new Fruit(new Point(0, 1 ));
+//		player.InitializeSnake(new Point(0, 0));
+//		player.moveToMyOwnDirection();
+//		game.CheckCollision(player, 1);
+//		Assert.assertEquals(4, player.getTailLenght());
+//	}
 	
 	@Test
 	public void outOfMap() {
-		Snake snake = new Snake();
-		Map map = new Map(800, 600);
+
 		snake.InitializeSnake(new Point(0,0));
 		snake.moveToLeft();
 		snake.moveToMyOwnDirection();
-		Assert.assertEquals(true, map.OutOfMap(snake));		
+		Assert.assertEquals(true, map2.OutOfMap(snake));		
 	}
 	
 	@Test
 	public void collideToFruit() {
-		Snake snake = new Snake();
-		Fruit fruit = new Fruit(new Point(1,0));
 		snake.InitializeSnake(new Point(0,0));
 		snake.moveToRight();
 		snake.moveToMyOwnDirection();
@@ -118,14 +124,13 @@ public class TestViborita {
 	
 	@Test
 	public void collideTosnake() {
-		Snake snake1 = new Snake();
-		Snake snake2 = new Snake();
-		snake1.InitializeSnake(new Point(0,0));
-		snake1.moveToRight();
-		snake1.moveToMyOwnDirection();
+
+		snake.InitializeSnake(new Point(0,0));
+		snake.moveToRight();
+		snake.moveToMyOwnDirection();
 		snake2.InitializeSnake(new Point(2,0));
 		snake2.moveToLeft();
 		snake2.moveToMyOwnDirection();
-		Assert.assertEquals(true, snake1.CollideTo(snake2));		
+		Assert.assertEquals(true, snake.CollideTo(snake2));		
 	}
 }
